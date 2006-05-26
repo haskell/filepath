9fc58a5bd01daf3c6788537c1a2a272d9a114d00
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2006-05-26 19:24:36 +0000

    Update with a new test architecture

--- Tests.hs
+++ Tests.hs
@@ -1,30 +1,48 @@
 
 module Main where
 
+-- import the relevant modules under test
 import System.FilePath
+--import qualified System.FilePath.Windows as W
+--import qualified System.FilePath.Posix as P
 
 
+-- framework support for the various tests
+data Test a = Test Int [Int]
+              deriving Show
 
-
-extension = 
-    [(getExtension "file.txt", ".txt")
-    ,(getExtension "file", "")
-    ,(getExtension "file/file.txt", ".txt")
-    ,(getExtension "file.txt/boris", "")
-    ,(getExtension "file.txt/boris.ext", ".ext")
-    ,(getExtension "file/path.txt.bob.fred", ".fred")
+instance Monad Test where
+    return _ = error "Not implemented"
+    _ >>= _  = error "Not implemented"
+    (Test c1 x1) >> (Test c2 x2) = Test (c1+c2) (x1 ++ map (+c1) x2)
     
-    ,(setExtension "file.txt" ".bob", "file.bob")
-    ,(setExtension "file.txt" "bob", "file.bob")
-    ,(setExtension "file" ".bob", "file.bob")
-    ,(setExtension "file.txt" "", "file")
-    ,(setExtension "file.fred.bob" "txt", "file.fred.txt")
+
+(===) :: Eq a => a -> a -> Test ()
+a === b | a == b = Test 1 []
+        | a /= b = Test 1 [1]
+
+
+tests = do
+    getExtension "file.txt" === ".txt"
+    getExtension "file" === ""
+    getExtension "file/file.txt" === ".txt"
+    getExtension "file.txt/boris" === ""
+    getExtension "file.txt/boris.ext" === ".ext"
+    getExtension "file/path.txt.bob.fred" === ".fred"
     
-    ,(dropExtension "file.txt", "file")
-    ,(dropExtension "file.txt/file.bob", "file.txt/file")
-    ,(dropExtension "file/file", "file/file")
-    ]
+    setExtension "file.txt" ".bob" === "file.bob"
+    setExtension "file.txt" "bob" === "file.bob"
+    setExtension "file" ".bob" === "file.bob"
+    setExtension "file.txt" "" === "file"
+    setExtension "file.fred.bob" "txt" === "file.fred.txt"
+    
+    dropExtension "file.txt" === "file"
+    dropExtension "file.txt/file.bob" === "file.txt/file"
+    dropExtension "file/file" === "file/file"
 
 
-
-main = print $ all (uncurry (==)) extension
+main = if null failed then
+            putStrLn $ "All tests passed (" ++ show count ++ ")"
+       else
+            putStrLn $ "FAILURES: " ++ show failed
+    where (Test count failed) = tests
