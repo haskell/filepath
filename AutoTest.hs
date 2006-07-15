fa6fc175320384f95f470dfa8b9b218132e4fd8b
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2006-07-15 14:12:25 +0000

    Add an auto-generate test architecture from the docs

---
+++ AutoTest.hs
@@ -0,0 +1,26 @@
+
+module AutoTest(module AutoTest, module Test.QuickCheck) where
+
+import Test.QuickCheck
+import Char
+
+
+constTest :: Bool -> IO ()
+constTest True = return ()
+constTest False = error "Failed on constTest"
+
+
+
+{-
+data QFilePath = QFilePath FilePath
+                 deriving Show
+
+instance Arbitrary QFilePath where
+    arbitrary = vector 25 >>= return . QFilePath
+
+-}
+
+
+instance Arbitrary Char where
+    arbitrary = oneof $ map return "?|./:\\abcd 123;_"
+
---
+++ GenTests.hs
@@ -0,0 +1,80 @@
+
+module Main where
+
+import System
+import List
+import Char
+
+
+data Test = Expr String
+          | Test [String] String
+          deriving Show
+
+
+main = do src <- readFile "System/FilePath.hs"
+          let tests = concatMap getTest $ zip [1..] (lines src)
+          writeFile "FilePath_Test.hs" (prefix ++ genTests tests)
+
+prefix = unlines
+    ["import AutoTest"
+    ,"import qualified System.FilePath.Windows as W"
+    ,"import qualified System.FilePath.Posix as P"
+    ,"main = do"
+    ]
+
+
+getTest :: (Int,String) -> [(Int,Test)]
+getTest (line,xs) | "-- > " `isPrefixOf` xs = f $ drop 5 xs
+    where
+        f x | "Windows:" `isPrefixOf` x = let res = grabTest (drop 8 x) in [g "W" res]
+            | "Posix:"   `isPrefixOf` x = let res = grabTest (drop 6 x) in [g "P" res]
+            | otherwise = let res = grabTest x in [g "W" res, g "P" res]
+
+        g p (Expr x) = (line,Expr (h p x))
+        g p (Test a x) = (line,Test a (h p x))
+        
+        h p x = joinLex $ map (addPrefix p) $ splitLex x
+
+getTest _ = []
+
+
+addPrefix :: String -> String -> String
+addPrefix pre str | all isAlpha str && length str > 1 && not (str `elem` prelude)
+                      = pre ++ "." ++ str
+                  | otherwise = str
+
+
+prelude = ["elem"]
+
+
+grabTest :: String -> Test
+grabTest x = if null free then Expr x else Test free x
+    where
+        free = nub [x | x <- lexs, length x == 1, all isAlpha x]
+        lexs = splitLex x
+
+
+
+splitLex :: String -> [String]
+splitLex x = case lex x of
+                [("","")] -> []
+                [(x,y)] -> x : splitLex y
+
+
+joinLex :: [String] -> String
+joinLex = unwords
+
+
+
+
+genTests :: [(Int, Test)] -> String
+genTests xs = unlines $ concatMap f $ zip [1..] xs
+    where
+        f (tno,(lno,test)) =
+            [" putStrLn \"Test " ++ show tno ++ ", from line " ++ show lno ++ "\""
+            ," " ++ genTest test]
+
+-- the result must be a line of the type "IO ()"
+genTest :: Test -> String
+genTest (Expr x) = "constTest (" ++ x ++ ")"
+genTest (Test free x) = "quickCheck (\\" ++ concatMap (' ':) free ++ " -> (" ++ x ++ "))"
