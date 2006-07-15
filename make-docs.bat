b35236db18c45528569e2a50b68ce9dfa74a662b
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2006-07-15 17:43:24 +0000

    Upgrade to HEAD versions of HsColour and Haddock, and used source hyperlinking between them

--- make-docs.bat
+++ make-docs.bat
@@ -5,10 +5,10 @@ copy System\FilePath.hs docs\System\FilePath.hs
 cpphs --noline System\FilePath\Windows.hs -D__HADDOCK__ > docs\System\FilePath\Windows.hs
 cpphs --noline System\FilePath\Posix.hs -D__HADDOCK__ > docs\System\FilePath\Posix.hs
 
-haddock -h docs\System\FilePath.hs docs\System\FilePath\Posix.hs docs\System\FilePath\Windows.hs --odir=docs
+haddock -h docs\System\FilePath.hs docs\System\FilePath\Posix.hs docs\System\FilePath\Windows.hs --odir=docs --lib=d:\bin --source-module=FilePath.html --source-entity=FilePath.html#%%{NAME}
 
 copy d:\bin\hscolour.css docs\hscolour.css
-hscolour -css System\FilePath.hs > docs\FilePath.html
+hscolour -anchorCSS System\FilePath.hs > docs\FilePath.html
 
 if "%1" == "deploy" copy "docs\*.*" "W:\projects\filepath\*.*"
 
