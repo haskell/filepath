33bb3ee3ea59fc6515cf110c0daa1f22859dbc00
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2006-07-19 12:49:44 +0000

    Fix up the flags in the test script

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
-ghc System\FilePath\Posix.hs -cpp -DTESTING
-ghc System\FilePath\Windows.hs -cpp -DTESTING
+ghc System\FilePath\Posix.hs -c -cpp -DTESTING
+ghc System\FilePath\Windows.hs -c -cpp -DTESTING
 runhaskell GenTests
 runhaskell FilePath_Test
