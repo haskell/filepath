d9493754b183e1c09b63543ed576ff75ee0746ef
Neil Mitchell <unknown>
2006-11-07 19:59:06 +0000

    Fix so that the --lib flag is baked into haddock, if you compile haddock properly

--- make-docs.bat
+++ make-docs.bat
@@ -4,7 +4,7 @@ copy System\FilePath\Compat.hs docs\System.FilePath.Compat.hs
 cpphs --noline System\FilePath\Windows.hs -D__HADDOCK__ > docs\System.FilePath.Windows.hs
 cpphs --noline System\FilePath\Posix.hs -D__HADDOCK__ > docs\System.FilePath.Posix.hs
 
-haddock -h docs\System.FilePath.hs docs\System.FilePath.Compat.hs docs\System.FilePath.Posix.hs docs\System.FilePath.Windows.hs --odir=docs --lib=d:\bin --source-module=%%{MODULE}.html --source-entity=%%{MODULE}.html#%%{NAME}
+haddock -h docs\System.FilePath.hs docs\System.FilePath.Compat.hs docs\System.FilePath.Posix.hs docs\System.FilePath.Windows.hs --odir=docs --source-module=%%{MODULE}.html --source-entity=%%{MODULE}.html#%%{NAME}
 
 copy d:\bin\hscolour.css docs\hscolour.css
 hscolour -anchorCSS docs\System.FilePath.hs > docs\System.FilePath.html
