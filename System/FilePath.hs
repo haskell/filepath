62f4df1ae006d35f48cd47304b15d92f6eca4657
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2006-11-08 13:38:40 +0000

    Add a wrapper for System.FilePath that picks the latest version

---
+++ System/FilePath.hs
@@ -0,0 +1,17 @@
+{- |
+Module      :  System.FilePath
+Copyright   :  (c) Neil Mitchell 2005-2006
+License     :  BSD3
+
+Maintainer  :  http://www.cs.york.ac.uk/~ndm/
+Stability   :  in-progress
+Portability :  portable
+
+A library for FilePath manipulations, this is a wrapper to pick the latest version
+of the libary. For the moment I suggest importing "System.FilePath.Version_0_10"
+directly, to avoid any future compatability concerns.
+-}
+
+module System.FilePath(module System.FilePath.Version_0_10) where
+
+import System.FilePath.Version_0_10
