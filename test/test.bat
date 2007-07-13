aea3a12343ba3e5fa89a8ec94ab39d5bdc0f160c
Neil Mitchell <unknown>
2007-07-13 22:02:42 +0000

    Refactor the test script, to support more options

--- test/test.bat
+++ test/test.bat
@@ -1,7 +1,32 @@
+@echo off
 
-set cmd=runhaskell
-if not "%1" == "" set cmd=ghci
+if "%1" == "help" goto help
 
+echo Generating test script
 runhaskell GenTests
-%cmd% -i.. -cpp -DTESTING FilePath_Test.hs
 
+if "%1" == "hpc" goto hpc
+if "%1" == "ghci" goto ghci
+
+runhaskell -i.. -cpp -DTESTING FilePath_Test.hs
+goto end
+
+
+:ghci
+ghci -i.. -cpp -DTESTING FilePath_Test.hs
+goto end
+
+
+:hpc
+echo Hpc compilation here
+goto end
+
+
+:help
+echo FilePath tester script
+echo   test            Run the tests using runhaskell
+echo   test ghci       Run the tests using ghci
+echo   test hpc        Run the tests using hpc
+goto end
+
+:end
