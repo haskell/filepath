654be5bc8baf2cf0efba5b176a5934323dc57832
Neil Mitchell <ndmitchell@gmail.com>
2014-11-03 10:50:14 +0000

    #37, let the generator take an argument of the number of tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,6 +1,7 @@
 
 module Test(main) where
 
+import System.Environment
 import TestGen
 import Control.Monad
 import Test.QuickCheck
@@ -8,10 +9,13 @@ import Test.QuickCheck
 
 main :: IO ()
 main = do
+    args <- getArgs
+    let count = case args of i:_ -> read i; _ -> 10000
+    putStrLn $ "Testing with " ++ show count ++ " repetitions"
     let total = length tests
     bs <- forM (zip [1..] tests) $ \(i,(msg,prop)) -> do
         putStrLn $ "Test " ++ show i ++ " of " ++ show total ++ ": " ++ msg
-        res <- quickCheckWithResult stdArgs{maxSuccess=10000} prop
+        res <- quickCheckWithResult stdArgs{maxSuccess=count} prop
         case res of
             Success{} -> return True
             _ -> putStrLn "TEST FAILURE!" >> return False
