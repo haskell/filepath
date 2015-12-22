ff4dba521bee9fb71d5f92c26c27740f8a5b6d7e
Neil Mitchell <ndmitchell@gmail.com>
2015-12-22 12:43:04 +0000

    #48, tweak the docs following suggestion from @rwbarton

--- System/FilePath/Internal.hs
+++ System/FilePath/Internal.hs
@@ -620,8 +620,8 @@ combineAlways a b | null a = b
 
 -- | Combine two paths with a path separator.
 --   If the second path starts with a path separator or a drive letter, then it returns the second.
---   The intention is that @setCurrentDirectory dir; readFile file@ will access the same file as
---   @readFile (dir '</>' file)@.
+--   The intention is that @readFile (dir '</>' file)@ will access the same file as
+--   @setCurrentDirectory dir; readFile file@.
 --
 -- > Posix:   "/directory" </> "file.ext" == "/directory/file.ext"
 -- > Windows: "/directory" </> "file.ext" == "/directory\\file.ext"
