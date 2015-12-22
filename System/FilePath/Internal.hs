afd45d015d431dc68e89b406cebc3fe32f337fb7
Neil Mitchell <ndmitchell@gmail.com>
2015-12-22 14:12:13 +0000

    GHC 7.8 and earlier compatibility

--- System/FilePath/Internal.hs
+++ System/FilePath/Internal.hs
@@ -1027,4 +1027,4 @@ breakEnd p = spanEnd (not . p)
 -- Nothing if the list did not end with the suffix given, or Just the list
 -- before the suffix, if it does.
 stripSuffix :: Eq a => [a] -> [a] -> Maybe [a]
-stripSuffix xs ys = reverse <$> stripPrefix (reverse xs) (reverse ys)
+stripSuffix xs ys = fmap reverse $ stripPrefix (reverse xs) (reverse ys)
